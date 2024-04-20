fac953d2bedd0ff93042fd055016c6d65ba78c99
Jhair7474 <marlonmoncayo7474@gmail.com>
2024-04-20 14:02:48 -0500

    Creacion de Server y Configuracion de Express

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hola, mundo desde Express!');
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  });
